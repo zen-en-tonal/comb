73cddfe3e3443f3f4f2e1f80ace0b69aa312a181
Takeru KODAMA <zen.en.tonal@gmail.com>
2025-09-28 07:39:23 +0000

    fix: update run and do_run to improve argument handling and error management in SingleFlight

--- lib/comb/caching/single_flight.ex
+++ lib/comb/caching/single_flight.ex
@@ -20,33 +20,45 @@ defmodule Comb.Caching.SingleFlight do
   end
 
   @spec run(name :: atom(), {module(), fun_name :: atom(), args :: [term()]}) ::
-          term() | {:error, term()}
+          {:ok, term()} | {:error, term()}
   def run(name, {module, fun_name, args}, timeout \\ 5_000)
       when is_atom(module) and is_atom(fun_name) do
+    scope = scope_for(name)
     group = group_for({module, fun_name}, args)
     closure = fn -> apply(module, fun_name, args) end
 
-    do_run(name, group, closure, timeout)
+    do_run(name, {scope, group}, closure, timeout)
   end
 
-  defp do_run(name, group, closure, timeout) do
-    Registry.via(name, __MODULE__)
-    |> GenServer.cast({:flight, group, closure, self()})
+  defp do_run(name, {scope, group}, closure, timeout) do
+    task =
+      Module.concat(name, TaskSup)
+      |> Task.Supervisor.async_nolink(fn ->
+        Registry.via(name, __MODULE__)
+        |> GenServer.cast({:flight, {scope, group}, closure, self()})
 
-    receive do
-      {:done, ^group, result} ->
-        :pg.leave(scope_for(name), group, self())
-        result
-    after
-      timeout ->
-        :pg.leave(scope_for(name), group, self())
-        {:error, :timeout}
+        receive do
+          {:done, ^group, result} ->
+            :pg.leave(scope, group, self())
+            result
+        after
+          timeout + 1_000 ->
+            :pg.leave(scope, group, self())
+            {:error, :timeout}
+        end
+      end)
+
+    case Task.yield(task, timeout) || Task.ignore(task) do
+      {:ok, {:ok, result}} -> {:ok, result}
+      {:ok, {:error, result}} -> {:error, result}
+      {:ok, result} -> {:ok, result}
+      nil -> {:error, :timeout}
+      {:exit, reason} -> {:error, reason}
     end
   end
 
   @impl true
-  def handle_cast({:flight, group, closure, caller}, %{name: name} = state) do
-    scope = scope_for(name)
+  def handle_cast({:flight, {scope, group}, closure, caller}, %{name: name} = state) do
     :ok = :pg.join(scope, group, caller)
 
     leader =
@@ -64,7 +76,7 @@ defmodule Comb.Caching.SingleFlight do
     {:noreply, state}
   end
 
-  defp group_for(fun, arg), do: {fun, arg}
+  defp group_for(fun, arg), do: {fun, :erlang.phash2(arg)}
 
   defp scope_for(name), do: Module.concat(name, "PG")
 
